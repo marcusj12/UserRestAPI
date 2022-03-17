73736cd79796979dad691183c796102818a7b4db
marcusj12 <marcus.a.jennings12@gmail.com>
2022-03-17 18:46:16 -0400

    UserRestAPI

---
+++ src/test/java/com/careerdevs/UserRestAPI/UserRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.UserRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
